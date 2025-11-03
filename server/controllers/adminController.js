34a7b5daec78c0abb027f2435a48167da0b3316a
Topher254 <raphaelsarota@gmail.com>
2025-11-03 23:08:57 +0300

    Code:Ready to connect to frontend

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,9 +1,11 @@
 import  jwt  from "jsonwebtoken";
+import Blog from "../models/blog.js";
+import Comment from "../models/comment.js";
 
 
  
 
-const adminLogin=async(req,res)=>{
+export const adminLogin=async(req,res)=>{
     try {
         const {email,password} = req.body;
         if (email!==process.env.ADMIN_EMAIL || password!==process.env.ADMIN_PASSWORD){
@@ -19,5 +21,72 @@ const adminLogin=async(req,res)=>{
     }
 }
 
+export const getAllBlogsAdmin=async(req,res)=>{
+    try {
+        const blogs = (await Blog.find({})).toSorted({createdAt:-1});
+        res.json({success:true,blogs})
 
-export default adminLogin;
+    } catch (error) {
+        res.json({message:error.message})
+        
+    }
+}
+
+
+// admin can see all comments whether approved or not
+export const getAllComments = async(req,res)=>{
+    try {
+        Comments = (await Comment.find({}).populate("blog")).toSorted({createdAt:-1});
+        res.json({success:true,Comments})
+
+    } catch (error) {
+        res.json({message:error.message})
+
+        
+    }
+}
+
+
+// dashboard data
+export const getDashboard = async(req,res)=>{
+    try {
+        const recentBlogs = await Blog.find({}).sort({createdAt:-1}).limit(5);
+        const blogs = await Blog.countDocuments();
+        const comments = await Comment.countDocuments();
+        const drafts = await Blog.countDocuments({isPublished:false});
+
+        const dashboardData={
+            blogs,comments,drafts
+        }
+        res.json({success:true,dashboardData})
+        
+    } catch (error) {
+        res.json({message:error.message})
+        
+    }
+}
+
+
+// admin can delete comment
+export const deleteCommentById=async(req,res)=>{
+    try {
+        const {id} = req.body;
+        await Comment.findByIdAndDelete(id);
+        res.status(200).json({message:"comment delted"})
+    } catch (error) {
+        
+    }
+}
+
+
+
+// approcve comments
+export const approveCommentById=async(req,res)=>{
+    try {
+        const {id} = req.body;
+        await Comment.findByIdAndUpdate(id,{isApproved:true});
+        res.status(200).json({message:"comment approved"})
+    } catch (error) {
+        
+    }
+}
--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -3,57 +3,133 @@ import fs from 'fs'
 import imageKit from "../config/imageKit.js";
 import ImageKit from "imagekit";
 import Blog from "../models/blog.js";
+import Comment from "../models/comment.js";
 
 // CREATE BLOG
-export const addBlog=async(req,res)=>{
+export const addBlog = async (req, res) => {
     try {
-        const {title,subTitle,description,category,isPublished} =JSON.parse(req.body.blog);
-        const imageFile=req.file;
+        const { title, subTitle, description, category, isPublished } = JSON.parse(req.body.blog);
+        const imageFile = req.file;
 
-        if(!title || !subTitle || !description ||!imageFile){
-            return res.json({success:false,message:"missing all fields"})
+        if (!title || !subTitle || !description || !imageFile) {
+            return res.json({ success: false, message: "missing all fields" })
         }
         const fileBuffer = fs.readFileSync(imageFile.path);
         // imagekit image ipload
         const response = await imageKit.upload({
-            file:fileBuffer,
-            fileName:imageFile.originalname,
-            folder:"/blogs"
+            file: fileBuffer,
+            fileName: imageFile.originalname,
+            folder: "/blogs"
         })
-// then optimize usansformation
-// ing url tr
-const optimizedImageURL = imageKit.url({
-    path: response.filePath,
-    transformation: [
-      { quality: 'auto' },
-      { format: 'webp' },
-      { width: '1280' }
-    ]
-});
+        // then optimize usansformation
+        // ing url tr
+        const optimizedImageURL = imageKit.url({
+            path: response.filePath,
+            transformation: [
+                { quality: 'auto' },
+                { format: 'webp' },
+                { width: '1280' }
+            ]
+        });
 
-// strorein avriable
-const image = optimizedImageURL;
+        // strorein avriable
+        const image = optimizedImageURL;
 
 
-await Blog.create({title,subTitle,description,category,image,isPublished});
-res.json({success:true,message:"Blog added"})
+        await Blog.create({ title, subTitle, description, category, image, isPublished });
+        res.json({ success: true, message: "Blog added" })
 
 
     } catch (error) {
-res.json({success:false,message:error.message || "something went wrong"})
+        res.json({ success: false, message: error.message || "something went wrong" })
 
-        
+
     }
 }
 
 // GET   BLOGS
 
-export const getAllBlogs=async(req,res)=>{
+export const getAllBlogs = async (req, res) => {
     try {
-        const blogs = await Blog.find({isPublished:true});
-        res.json({success:true,blogs})
+        const blogs = await Blog.find({ isPublished: true });
+        res.json({ success: true, blogs })
     } catch (error) {
-        res.json({success:false,"message":error.message})
+        res.json({ success: false, "message": error.message })
     }
 
+}
+
+
+// blog by id
+export const getBlogById = async (req, res) => {
+    try {
+        const { blogId } = req.params;
+        const blog = await Blog.findById(blogId);
+        if (!blog) {
+            return res.status(404).json({ "message": "blog not found" })
+        }
+        res.status(200).json(blog)
+    } catch (error) {
+        res.status(404).json({ "message": error.message })
+    }
+}
+
+
+// delete a blog
+export const deleteBlogById = async (req, res) => {
+    try {
+        const { id } = req.body;
+        await Blog.findByIdAndDelete(id)
+
+        // delete all comments asscoiated
+        await Comment.deleteMany({ blog: id })
+
+
+
+        res.status(200).json({ "message": "blog deleted" })
+    } catch (error) {
+        res.status(500).json({ "message": error.message })
+    }
+}
+
+
+// publish or unpublish
+export const togglePublish = async (req, res) => {
+    try {
+        const { id } = req.body;
+        const blog = await Blog.findById(id);
+        blog.isPublished = !blog.isPublished;
+        await blog.save()
+        res.status(200).json({ "message": "publish edited" })
+
+    } catch (error) {
+        res.status(500).json({ "message": error.message })
+
+    }
+}
+
+// add comment to a blog
+export const addComment = async (req, res) => {
+    try {
+        const { blog, name, content } = req.body;
+        await Comment.create({ blog, name, content });
+        res.status(200).json({ message: "comment added for review" })
+
+    } catch (error) {
+        res.status(500).json({ "message": error.message })
+
+    }
+}
+
+
+export const getBlogComments = async (req, res) => {
+    try {
+        const { blogId } = req.body;
+        const comments = (await Comment.find({ blog: blogId, isApproved: true })).toSorted({ createdAt: -1 });
+        res.status(200).json(comments)
+
+    } catch (error) {
+        res.status(500).json({ "message": error.message })
+
+    }
 }
